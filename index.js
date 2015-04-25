0353dc1aef573416a09b13395c7acaf60f88f77d
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-04-25 19:18:30 +0300

    Project created. Server command added.

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var program = require('commander');
+var path = require('path');
+var pkg = require('./package');
+var fs = require('fs');
+var yaml = require('js-yaml');
+var gizi = require('./lib/gizi');
+
+program
+  .version(pkg.version);
+
+program
+  .command('server')
+  .usage('starts a Gizi server in current directory')
+  .description('Starts a Gizi server in current directory')
+  .action(function () {
+    var currentPath = path.resolve(process.cwd());
+    var config = yaml.safeLoad(fs.readFileSync(currentPath + '/.gizi.yml'));
+    var projectPath = path.resolve(currentPath, config.path || '');
+    var ip = config.ip || '0.0.0.0';
+    var port = config.port || '8895';
+    gizi.server(projectPath, {
+      ip: ip,
+      port: port
+    }, function () {
+      console.log('Server started');
+    });
+  });
+
+program.parse(process.argv);
