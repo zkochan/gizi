b85ba04f06ccc0d93a69f0957b4bb4beb18e7b36
Zoltan Kochan <zoltan.kochan+github@gmail.com>
2015-05-23 20:00:32 +0300

    Defaults added.

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ var fs = require('fs');
 var yaml = require('js-yaml');
 var gizi = require('./lib/gizi');
 
+var defaultConfig = {
+  sourceFolderName: './src',
+  siteFolderName: './site',
+  buildFolderName: './build',
+  port: 9595,
+  ip: '0.0.0.0'
+};
+
 program
   .version(pkg.version);
 
@@ -17,18 +25,19 @@ program
   .action(function() {
     var currentPath = path.resolve(process.cwd());
     var config = yaml.safeLoad(fs.readFileSync(currentPath + '/.gizi.yml'));
-    var destinationPath = path.join(currentPath, './release');
+    var destinationPath = path.join(currentPath, defaultConfig.siteFolderName);
+    var buildFolder = config.buildFolder || defaultConfig.buildFolderName;
 
     if (config.source === 'git') {
       gizi.gitServer(path.join(currentPath, './src'), {
         repoUrl: config.url,
         cmds: config.cmds || [],
-        source: path.join(currentPath, './src', config.buildFolder || './build'),
+        source: path.join(currentPath, defaultConfig.sourceFolderName, buildFolder),
         destination: destinationPath
       });
       gizi.server(destinationPath, {
-        ip: config.ip || '0.0.0.0',
-        port: config.port || 9595
+        ip: config.ip || defaultConfig.ip,
+        port: config.port || defaultConfig.port
       }, function() {
         console.log('Server started');
       });
